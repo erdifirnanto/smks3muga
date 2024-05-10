b0603c12c79068151c9ea9a61d65c4df414c233f
erdifirnanto <erdifirnanto@gmail.com>
2024-05-10 23:52:36 +0700

    smk 3 muga new

---
+++ resources/views/user/footer.blade.php
@@ -0,0 +1,11 @@
+<footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <p>Copyright © 2021 SEO Dream Co., Ltd. All Rights Reserved. 
+          
+          <br>Web Designed by <a rel="nofollow" href="https://templatemo.com" title="free CSS templates">TemplateMo</a></p>
+        </div>
+      </div>
+    </div>
+  </footer>
---
+++ resources/views/user/header.blade.php
@@ -0,0 +1,31 @@
+<header class="header-area header-sticky wow slideInDown" data-wow-duration="0.75s" data-wow-delay="0s">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <nav class="main-nav">
+            <!-- ***** Logo Start ***** -->
+            <a href="index.html" class="logo">
+              <h4><img src="{{asset('user/assets/images/logo_muga.png')}}" style="width:80px" alt="">SMK 3 MUGA</h4>
+            </a>
+            <!-- ***** Logo End ***** -->
+            <!-- ***** Menu Start ***** -->
+            <ul class="nav">
+              <li class="scroll-to-section"><a href="#top" class="active">Home</a></li>
+              <li class="scroll-to-section"><a href="#features">Profile</a></li>
+              <li class="scroll-to-section"><a href="#about">Informasi</a></li>
+              <li class="scroll-to-section"><a href="#services">Jurusan</a></li>
+              <li class="scroll-to-section"><a href="#">Ekskul</a></li>
+              <li class="scroll-to-section"><a href="#galery">Galery</a></li> 
+              <li class="scroll-to-section"><a href="#contact">Kontak</a></li> 
+              <li class="scroll-to-section"><a href="#contact">PPDB</a></li> 
+              <!-- <li class="scroll-to-section"><div class="main-blue-button"><a href="#contact">Get Your Quote</a></div></li>  -->
+            </ul>        
+            <a class='menu-trigger'>
+                <span>Menu</span>
+            </a>
+            <!-- ***** Menu End ***** -->
+          </nav>
+        </div>
+      </div>
+    </div>
+  </header>
