16baf877d6b626f7c2e55c62f421f6ac37d866d3
erdifirnanto <erdifirnanto@gmail.com>
2024-05-14 23:23:02 +0700

    ubah_deskripsi

--- resources/views/user/footer.blade.php
+++ resources/views/user/footer.blade.php
@@ -2,9 +2,9 @@
     <div class="container">
       <div class="row">
         <div class="col-lg-12">
-          <p>Copyright © 2021 SEO Dream Co., Ltd. All Rights Reserved. 
+          <p>Copyright © 2024 Erdi Vernanto & Rena Amalia Afifah. 
           
-          <br>Web Designed by <a rel="nofollow" href="https://templatemo.com" title="free CSS templates">TemplateMo</a></p>
+          <!-- <br>Web Designed by <a rel="nofollow" href="https://templatemo.com" title="free CSS templates">TemplateMo</a></p> -->
         </div>
       </div>
     </div>
