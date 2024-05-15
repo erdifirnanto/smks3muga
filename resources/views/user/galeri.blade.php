1d6c956f1b26fcf159d28d9f929747efb44dd15d
erdifirnanto <erdifirnanto@gmail.com>
2024-05-15 23:58:02 +0700

    update_profile

--- resources/views/user/galeri.blade.php
+++ resources/views/user/galeri.blade.php
@@ -1,6 +1,119 @@
 @extends('user.master')
 @section('master')
 
+<div class="main-banner wow fadeIn" id="top" data-wow-duration="1s" data-wow-delay="0.5s">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-14">
+          <div class="row">
+            <div class="col-lg-10 align-self-center">
+              <div class="left-content header-text wow fadeInLeft" data-wow-duration="1s" data-wow-delay="1s">
+                <div class="row">
+                  <div class="col-lg-7 col-sm-7">
+                    <div class="info-stat">
+                    <h3>Selamat Datang di Kampus</h3>
+                    <h3>SMKS Muhammadiyah 3 Tegaldlimo</h3>
+                    <!-- <h6>Agency Status:</h6> -->
+                    </div>
+                  </div>
+                  <!-- <div class="col-lg-4 col-sm-4">
+                    <div class="info-stat">
+                      <h6>Price:</h6>
+                      <h4>$720/Month</h4>
+                    </div>
+                  </div>
+                  <div class="col-lg-4 col-sm-4">
+                    <div class="info-stat">
+                      <h6>Schedules</h6>
+                      <h4>$450/Meeting</h4>
+                    </div>
+                  </div> -->
+                  <!-- <div class="col-lg-12">
+                    <h2>SEO &amp; Digital Marketing Agency</h2>
+                  </div> -->
+                  <div class="col-lg-12 mt-4">
+                    <div class="main-green-button scroll-to-section">
+                      <a href="#contact">PPDB</a>
+                    </div>
+                  </div>
+                  <div class="col-lg-7 col-sm-7 mt-4">
+                    <div class="info-stat">
+                    <h4>ISLAMI SANTUN & PROFESIONAL</h4>
+                    <!-- <h3>SMKS Muhammadiyah 3 Tegaldlimo</h3> -->
+                    <!-- <h6>Agency Status:</h6> -->
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- <div class="col-lg-5">
+              <div class="right-image wow fadeInRight" data-wow-duration="1s" data-wow-delay="0.5s">
+                <img src="{{asset('user/assets/images/banner-right-image.png')}}" alt="">
+              </div>
+            </div> -->
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div id="features" class="features section">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="features-content">
+            <div class="row">
+              <div class="col-lg-3">
+                <div class="features-item first-feature wow fadeInUp" data-wow-duration="1s" data-wow-delay="0s">
+                  <!-- <div class="first-number number">
+                    <h6>01</h6>
+                  </div> -->
+                  <div class="icon"></div>
+                  <h4>Pengembangan Perangkat Lunak & Gim (PPLG)</h4>
+                  <div class="line-dec"></div>
+                  <p>Jurusan PPLG mempelajari tentang perkembangan perangkat lunak meliputi pembuatan, manajemen organisasi pengembangan perangkat lunak, pemeliharaan dan manajemen kualitas.s</p>
+                </div>
+              </div>
+              <div class="col-lg-3">
+                <div class="features-item second-feature wow fadeInUp" data-wow-duration="1s" data-wow-delay="0.2s">
+                  <!-- <div class="second-number number">
+                    <h6>02</h6>
+                  </div> -->
+                  <div class="icon"></div>
+                  <h4>Otomasi Tata Kelola Perkantoran (OTKP)</h4>
+                  <div class="line-dec"></div>
+                  <p>Jurusan OTKP mempelajari segala jenis kegiatan kantor. Mulai dari pembukuan, pengarsipan, hingga public relations.</p>
+                </div>
+              </div>
+              <div class="col-lg-3">
+                <div class="features-item first-feature wow fadeInUp" data-wow-duration="1s" data-wow-delay="0.4s">
+                  <!-- <div class="third-number number">
+                    <h6>03</h6>
+                  </div> -->
+                  <div class="icon"></div>
+                  <h4>Teknik Bisnis Sepeda Motor (TBSM)</h4>
+                  <div class="line-dec"></div>
+                  <p>If this template is useful for your website, please consider to <a rel="nofollow" href="https://www.paypal.me/templatemo" target="_blank">support us</a> a little.</p>
+                </div>
+              </div>
+              <div class="col-lg-3">
+                <div class="features-item second-feature last-features-item wow fadeInUp" data-wow-duration="1s" data-wow-delay="0.6s">
+                  <!-- <div class="fourth-number number">
+                    <h6>04</h6>
+                  </div> -->
+                  <div class="icon"></div>
+                  <h4>Teknik Kendaraan Ringan (TKRO)</h4>
+                  <div class="line-dec"></div>
+                  <p>Jurusan TKRO adalah salah satu bidang profesi yang mempelajari kompetisi keahlian bidang teknik otomotif yang menekankan pada bidang jasa perbaikan kendaraan ringan (Mobil).</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
   <div id="about" class="about-us section">
     <div class="container">
       <div class="row">
@@ -52,7 +165,8 @@
       <div class="row">
         <div class="col-lg-6 offset-lg-3">
           <div class="section-heading wow bounceIn" data-wow-duration="1s" data-wow-delay="0.2s">
-            <h3>Daftar Guru</h3>
+            <h6>Our Services</h6>
+            <h2>Discover What We Do &amp; <span>Offer</span> To Our <em>Clients</em></h2>
           </div>
         </div>
       </div>
@@ -183,7 +297,7 @@
             <div class="item">
               <div class="portfolio-item">
                 <div class="thumb">
-                  <img src="{{asset('user/assets/images/kepsek.jpg')}}" alt="">
+                  <img src="{{asset('user/assets/images/portfolio-01.jpg')}}" alt="">
                   <div class="hover-content">
                     <div class="inner-content">
                       <a href="#"><h4>Awesome Project 101</h4></a>
@@ -194,7 +308,7 @@
               </div>
               <div class="portfolio-item">
                 <div class="thumb">
-                  <img src="{{asset('user/assets/images/gedung.jpg')}}" alt="">
+                  <img src="{{asset('user/assets/images/portfolio-04.jpg')}}" alt="">
                   <div class="hover-content">
                     <div class="inner-content">
                       <a href="#"><h4>Awesome Project 102</h4></a>
@@ -242,7 +356,7 @@
               </div>
               <div class="portfolio-item">
                 <div class="thumb">
-                  <img src="{{asset('user/assets/images/gedung.jpg')}}" alt="">
+                  <img src="{{asset('user/assets/images/portfolio-06.jpg')}}" alt="">
                   <div class="hover-content">
                     <div class="inner-content">
                       <a href="#"><h4>Awesome Project 106</h4></a>
