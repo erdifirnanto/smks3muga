89800d58eaed7a3d1a2f3d44e839f5a96af5bd45
renaamaliafif <renaamalia789@gmail.com>
2024-05-26 01:58:53 +0700

    lupa

--- resources/views/user/ekskul.blade.php
+++ resources/views/user/ekskul.blade.php
@@ -6,162 +6,18 @@
     <div class="row">
       <div class="col-lg-6">
         <div class="left-image wow fadeInLeft" data-wow-duration="1s" data-wow-delay="0.5s">
-          <img style="width:300px;" src="{{asset('user/assets/images/kepsek.jpg')}}" alt="">
+          <img style="width:300px;" src="{{asset('user/assets/images/ipm.jpg')}}" alt="">
         </div>
       </div>
       <div class="col-lg-6 align-self-center wow fadeInRight" data-wow-duration="1s" data-wow-delay="0.5s">
         <div class="section-heading">
           <!-- <h6>About Us</h6> -->
-          <!-- <h2>Top <em>marketing</em> agency &amp; consult your website <span>with us</span></h2> -->
-          <h2>Sambutan Kepala Sekolah</h2>
+          <h3>Ikatan Pelajar Muhammadiyah</h3>
         </div>
-        <!-- <div class="row">
-            <div class="col-lg-4 col-sm-4">
-              <div class="about-item">
-                <h4>750+</h4>
-                <h6>projects finished</h6>
-              </div>
-            </div>
-            <div class="col-lg-4 col-sm-4">
-              <div class="about-item">
-                <h4>340+</h4>
-                <h6>happy clients</h6>
-              </div>
-            </div>
-            <div class="col-lg-4 col-sm-4">
-              <div class="about-item">
-                <h4>128+</h4>
-                <h6>awards</h6>
-              </div>
-            </div>
-          </div> -->
-        <p>
-          <!-- <a rel="nofollow" href="https://templatemo.com/tm-563-seo-dream" target="_parent">SEO Dream</a> -->
-        <div style="text-align:justify">
-          Selamat datang di website SMK Muhammadiyah 3 Tegaldlimo yang saya tujukan untuk seluruh unsur pimpinan, guru, karyawan dan siswa serta khalayak umum guna dapat mengakses seluruh informasi tentang sekolah kami. Tentunya dalam penyajian informasi masih banyak kekurangan, oleh karena itu kepada seluruh civitas akademika dan masyarakat umum dapat memberikan saran dan kritik demi kemajuan lebih lanjut.
-          Saya berharap Website ini dapat dijadikan wahana interaksi yang positif baik antar civitas akademika maupun masyarakat pada umumnya, sehingga dapat menjalin silaturahmi yang erat di segala unsur. Mari kita bekerja dan berkarya dengan mengharap ridho sang Kuasa dan keikhlasan yang tulus demi anak bangsa.
-        </div>
-        </p>
-        <h6><b>RAHMI NUR AINI, M.Pd. </h6>
-        <!-- <div class="main-green-button mt-4"><a href="#">Discover company</a></div> -->
-      </div>
-    </div>
-  </div>
-</div>
 
-<div id="services" class="our-services section">
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-6 offset-lg-3">
-        <div class="section-heading wow bounceIn" data-wow-duration="1s" data-wow-delay="0.2s">
-          <h3>Daftar Guru</h3>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-lg-4">
-        <div class="service-item wow bounceInUp" data-wow-duration="1s" data-wow-delay="0.3s">
-          <div class="row">
-            <div class="col-lg-4">
-              <div class="icon">
-                <img src="{{asset('user/assets/images/service-icon-01.png')}}" alt="">
-              </div>
-            </div>
-            <div class="col-lg-8">
-              <div class="right-content">
-                <h4>Similar Websites</h4>
-                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium dormque laudantium.</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-4">
-        <div class="service-item wow bounceInUp" data-wow-duration="1s" data-wow-delay="0.4s">
-          <div class="row">
-            <div class="col-lg-4">
-              <div class="icon">
-                <img src="assets/images/service-icon-02.png" alt="">
-              </div>
-            </div>
-            <div class="col-lg-8">
-              <div class="right-content">
-                <h4>Website Trends</h4>
-                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium dormque laudantium.</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-4">
-        <div class="service-item wow bounceInUp" data-wow-duration="1s" data-wow-delay="0.5s">
-          <div class="row">
-            <div class="col-lg-4">
-              <div class="icon">
-                <img src="assets/images/service-icon-03.png" alt="">
-              </div>
-            </div>
-            <div class="col-lg-8">
-              <div class="right-content">
-                <h4>Traffic Analysis</h4>
-                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium dormque laudantium.</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-4">
-        <div class="service-item wow bounceInUp" data-wow-duration="1s" data-wow-delay="0.6s">
-          <div class="row">
-            <div class="col-lg-4">
-              <div class="icon">
-                <img src="assets/images/service-icon-03.png" alt="">
-              </div>
-            </div>
-            <div class="col-lg-8">
-              <div class="right-content">
-                <h4>Optimizing Keywords</h4>
-                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium dormque laudantium.</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-4">
-        <div class="service-item wow bounceInUp" data-wow-duration="1s" data-wow-delay="0.7s">
-          <div class="row">
-            <div class="col-lg-4">
-              <div class="icon">
-                <img src="assets/images/service-icon-01.png" alt="">
-              </div>
-            </div>
-            <div class="col-lg-8">
-              <div class="right-content">
-                <h4>Page Optimizations</h4>
-                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium dormque laudantium.</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-4">
-        <div class="service-item wow bounceInUp" data-wow-duration="1s" data-wow-delay="0.8s">
-          <div class="row">
-            <div class="col-lg-4">
-              <div class="icon">
-                <img src="{{asset('user/assets/images/service-icon-02.png')}}" alt="">
-              </div>
-            </div>
-            <div class="col-lg-8">
-              <div class="right-content">
-                <h4>Deep URL Analysis</h4>
-                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium dormque laudantium.</p>
-              </div>
-            </div>
-          </div>
-        </div>
+        <p>
+        <div style="text-align:justify">
+          Ikatan Pelajar Muhammadiyah merupakan </div></p>
       </div>
     </div>
   </div>
