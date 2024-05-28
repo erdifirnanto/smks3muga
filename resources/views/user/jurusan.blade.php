85fb3df010a7b552b1256219e6a2782432118af2
renaamaliafif <renaamalia789@gmail.com>
2024-05-29 01:12:11 +0700

    lupa

--- resources/views/user/jurusan.blade.php
+++ resources/views/user/jurusan.blade.php
@@ -6,87 +6,42 @@
     <div class="row">
       <div class="col-lg-6">
         <div class="left-image wow fadeInLeft" data-wow-duration="1s" data-wow-delay="0.5s">
-          <img style="width:300px;" src="{{asset('user/assets/images/praktikpplg.jpg')}}" alt="">
+          <img style="width:500px;" src="{{asset('user/assets/images/praktikpplg.jpg')}}" alt="">
         </div>
       </div>
       <div class="col-lg-6 align-self-center wow fadeInRight" data-wow-duration="1s" data-wow-delay="0.5s">
         <div class="section-heading">
-          <!-- <h6>About Us</h6> -->
-          <h3>Pengembangan Perangkat Lunak & Gim</h3>
+          <h2>Pengembangan Perangkat Lunak & Gim</h2>
         </div>
-
         <p>
         <div style="text-align:justify">
           Jurusan PPLG mempelajari tentang perkembangan perangkat lunak meliputi pembuatan, manajemen organisasi 
-          pengembangan perangkat lunak, pemeliharaan dan manajemen kualitas. </div>
-        </p>
+          pengembangan perangkat lunak, pemeliharaan dan manajemen kualitas.
+        </div>
+      </div>
+    </div>
+
+    <div id="about" class="about-us section">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6">
+            <div class="right-image wow fadeInRight" data-wow-duration="1s" data-wow-delay="0.5s">
+              <img style="width:500px;" src="{{asset('user/assets/images/praktikmplb.jpg')}}" alt="">
+            </div>
+          </div>
+      <div class="col-lg-6 align-self-center wow fadeInLeft" data-wow-duration="1s" data-wow-delay="0.5s">
+        <div class="section-heading">
+          <h2>Manajemen Perkantoran dan Layanan Bisnis</h2>
+        </div>
+        <p>
+        <div style="text-align:justify">
+          Jurusan MPLB mempelajari segala jenis kegiatan kantor. Mulai dari pembukuan, pengarsipan, hingga public relations.
+        </div>
       </div>
     </div>
   </div>
 </div>
-
-<div id="about" class="about-us section">
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-6">
-        <div class="left-image wow fadeInLeft" data-wow-duration="1s" data-wow-delay="0.5s">
-          <img style="width:300px;" src="{{asset('user/assets/images/praktikmplb.jpg')}}" alt="">
-        </div>
-      </div>
-      <div class="col-lg-6 align-self-center wow fadeInRight" data-wow-duration="1s" data-wow-delay="0.5s">
-        <div class="section-heading">
-          <!-- <h6>About Us</h6> -->
-          <h3>Manajemen Perkantoran dan Layanan Bisnis</h3>
-        </div>
-
-        <p>
-        <div style="text-align:justify">
-          Jurusan MPLB mempelajari segala jenis kegiatan kantor. Mulai dari pembukuan, pengarsipan, hingga public relations.</div></p>
-      </div>
-    </div>
-  </div>
 </div>
-
-<div id="about" class="about-us section">
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-6">
-        <div class="left-image wow fadeInLeft" data-wow-duration="1s" data-wow-delay="0.5s">
-          <img style="width:300px;" src="{{asset('user/assets/images/praktiktbsm.jpg')}}" alt="">
-        </div>
-      </div>
-      <div class="col-lg-6 align-self-center wow fadeInRight" data-wow-duration="1s" data-wow-delay="0.5s">
-        <div class="section-heading">
-          <!-- <h6>About Us</h6> -->
-          <h3>Teknik Bisnis Sepeda Motor</h3>
-        </div>
-
-        <p>
-        <div style="text-align:justify">
-          Jurusan TBSM </div></p>
-      </div>
-    </div>
-  </div>
 </div>
-
-<div id="about" class="about-us section">
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-6">
-        <div class="left-image wow fadeInLeft" data-wow-duration="1s" data-wow-delay="0.5s">
-          <img style="width:300px;" src="{{asset('user/assets/images/praktiktkro.jpg')}}" alt="">
-        </div>
-      </div>
-      <div class="col-lg-6 align-self-center wow fadeInRight" data-wow-duration="1s" data-wow-delay="0.5s">
-        <div class="section-heading">
-          <!-- <h6>About Us</h6> -->
-          <h3>Teknik Kendaraan Ringan Otomotif</h3>
-        </div>
-
-        <p>
-        <div style="text-align:justify">
-          Jurusan TKRO adalah salah satu bidang profesi yang mempelajari kompetisi keahlian bidang teknik otomotif yang menekankan pada bidang jasa perbaikan kendaraan ringan (Mobil). </div></p>
-    </div>
-  </div>
 </div>
 @endsection
