71ea6c26df6808a5dc34d71c0ff7ae8a40c16508
erdifirnanto <erdifirnanto@gmail.com>
2024-05-29 01:34:42 +0700

    jurusan

--- resources/views/user/jurusan.blade.php
+++ resources/views/user/jurusan.blade.php
@@ -4,40 +4,82 @@
 <div id="about" class="about-us section">
   <div class="container">
     <div class="row">
-      
-    <div class="col-lg-6">
+      <div class="col-lg-6">
         <div class="left-image wow fadeInLeft" data-wow-duration="1s" data-wow-delay="0.5s">
           <img style="width:500px;" src="{{asset('user/assets/images/praktikpplg.jpg')}}" alt="">
         </div>
       </div>
       <div class="col-lg-6 align-self-center wow fadeInRight" data-wow-duration="1s" data-wow-delay="0.5s">
-        <div class="section-heading">
+        <div style="text-align: center;" class="section-heading">
           <h2>Pengembangan Perangkat Lunak & Gim</h2>
         </div>
         <p>
         <div style="text-align:justify">
-          Jurusan PPLG mempelajari tentang perkembangan perangkat lunak meliputi pembuatan, manajemen organisasi 
+          Jurusan PPLG mempelajari tentang perkembangan perangkat lunak meliputi pembuatan, manajemen organisasi
           pengembangan perangkat lunak, pemeliharaan dan manajemen kualitas.
         </div>
       </div>
     </div>
 
-    <div id="about" class="about-us section">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-6">
-            <div class="right-image wow fadeInRight" data-wow-duration="1s" data-wow-delay="0.5s">
-              <img style="width:500px;" src="{{asset('user/assets/images/praktikmplb.jpg')}}" alt="">
-            </div>
+    <div style="margin-top: 100px;" class="row">
+      <div class="col-lg-6">
+        <div class="left-image wow fadeInLeft" data-wow-duration="1s" data-wow-delay="0.5s">
+          <div style="text-align: center;" class="section-heading">
+            <h2>Manajemen Perkantoran dan Layanan Bisnis</h2>
           </div>
-      <div class="col-lg-6 align-self-center wow fadeInLeft" data-wow-duration="1s" data-wow-delay="0.5s">
-        <div class="section-heading">
-          <h2>Manajemen Perkantoran dan Layanan Bisnis</h2>
+          <p>
+          <div style="text-align:justify">
+            Jurusan MPLB mempelajari segala jenis kegiatan kantor. Mulai dari pembukuan, pengarsipan, hingga public relations.
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-6 align-self-center wow fadeInRight" data-wow-duration="1s" data-wow-delay="0.5s">
+        <img style="width:550px;" src="{{asset('user/assets/images/praktikmplb.jpg')}}" alt="">
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+</div>
+</div>
+
+
+<!-- Isi dibawah ini ya cil -->
+
+
+<div id="about" class="about-us section">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-6">
+        <div class="left-image wow fadeInLeft" data-wow-duration="1s" data-wow-delay="0.5s">
+          <img style="width:500px;" src="{{asset('user/assets/images/praktikpplg.jpg')}}" alt=""> <!-- Ini Foto -->
+        </div>
+      </div>
+      <div class="col-lg-6 align-self-center wow fadeInRight" data-wow-duration="1s" data-wow-delay="0.5s">
+        <div style="text-align: center;" class="section-heading">
+          <h2>Judul</h2>
         </div>
         <p>
         <div style="text-align:justify">
-          Jurusan MPLB mempelajari segala jenis kegiatan kantor. Mulai dari pembukuan, pengarsipan, hingga public relations.
+          Deskripsi
         </div>
+      </div>
+    </div>
+
+    <div style="margin-top: 100px;" class="row">
+      <div class="col-lg-6">
+        <div class="left-image wow fadeInLeft" data-wow-duration="1s" data-wow-delay="0.5s">
+          <div style="text-align: center;" class="section-heading">
+            <h2>Judul Isi dah</h2>
+          </div>
+          <p>
+          <div style="text-align:justify">
+            Deskripsi isi
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-6 align-self-center wow fadeInRight" data-wow-duration="1s" data-wow-delay="0.5s">
+        <img style="width:550px;" src="{{asset('user/assets/images/praktikmplb.jpg')}}" alt=""> <!--Fotonya Disini Cil-->
       </div>
     </div>
   </div>
