d5c92649a43d132372a2f2e4a0b423912b455f33
renaamaliafif <renaamalia789@gmail.com>
2024-07-26 14:50:53 +0700

    visimisi

--- resources/views/user/visimisi.blade.php
+++ resources/views/user/visimisi.blade.php
@@ -4,9 +4,10 @@
 <div id="about" class="about-us section">
   <div class="container">
     <div class="row">
+      <!-- Visi Sekolah Section -->
       <div class="col-lg-6">
         <div class="left-image wow fadeInLeft" data-wow-duration="1s" data-wow-delay="0.5s">
-          <img style="width:500px;" src="{{asset('user/assets/images/gurukaryawan.jpg')}}" alt="">
+          <img style="width:500px;" src="{{ asset('user/assets/images/gurukaryawan.jpg') }}" alt="Visi Sekolah Image">
         </div>
       </div>
       <div class="col-lg-6 align-self-center wow fadeInRight" data-wow-duration="1s" data-wow-delay="0.5s">
@@ -20,30 +21,55 @@
       </div>
     </div>
 
+    <!-- Misi Sekolah Section -->
     <div style="margin-top: 60px;" class="row">
       <div class="col-lg-6">
         <div class="left-image wow fadeInLeft" data-wow-duration="1s" data-wow-delay="0.5s">
-          <div style="text-align: center;" class="section-heading">
-            <h2>Misi Sekolah</h2>
-          </div>
-
+          <img style="width:500px;" src="{{ asset('user/assets/images/gurukaryawan.jpg') }}" alt="Misi Sekolah Image">
         </div>
       </div>
       <div class="col-lg-6 align-self-center wow fadeInRight" data-wow-duration="1s" data-wow-delay="0.5s">
-        <div style="text-align:left">
-          <div> 1. Melaksanakan pendidikan berdasarkan Iman, Islam dan Taqwa </div>
-          <div> 2. Membentuk karakter pribadi siswa melalui ibadah </div>
-          <div> 3. Melaksanakan pendidikan yang unggul dibidang IPTEK </div>
-          <div> 4. Mencetak lulusan yang kompeten </div>
-          <div> 5. Membekali lulusan berwawasan kewirausahaan </div>
+        <div class="section-heading">
+          <h2>Misi Sekolah</h2>
+        </div>
+        <div style="text-align:left; margin-top: 20px;">
+          <div>1. Melaksanakan pendidikan berdasarkan Iman, Islam dan Taqwa</div>
+          <div>2. Membentuk karakter pribadi siswa melalui ibadah</div>
+          <div>3. Melaksanakan pendidikan yang unggul dibidang IPTEK</div>
+          <div>4. Mencetak lulusan yang kompeten</div>
+          <div>5. Membekali lulusan berwawasan kewirausahaan</div>
         </div>
       </div>
     </div>
 
+    <!-- Tujuan Section -->
+     <div style="margin-top: 60px;" class="row">
+      <div class="col-lg-6">
+        <div class="left-image wow fadeInLeft" data-wow-duration="1s" data-wow-delay="0.5s">
+          <img style="width:500px;" src="{{ asset('user/assets/images/gurukaryawan.jpg') }}" alt="Tujuan Sekolah Image">
+        </div>
+      </div>
+      <div class="col-lg-6 align-self-center wow fadeInRight" data-wow-duration="1s" data-wow-delay="0.5s">
+        <div class="section-heading">
+          <h2>Tujuan Sekolah</h2>
+        </div>
+        <div style="text-align:left; margin-top: 20px;">
+          <ul>
+            <li>Terwujudnya SMK Muhammadiyah 3 Tegaldlimo sebagai lembaga unggul dan menjadi pilihan masyarakat.</li>
+            <li>Terwujudnya lulusan SMK Muhammadiyah 3 Tegaldlimo yang memiliki daya saing yang tinggi untuk memasuki persaingan global.</li>
+            <li>Terbinanya potensi diri anak lewat pengembangan bakat minat dalam kegiatan ekstrakurikuler.</li>
+            <li>Terwujudnya kegiatan intra dan eksrakurikuler yang mampu mengembangkan potensi diri, perilaku kreatif sehingga menghasilkan prestasi yang unggul.</li>
+            <li>Tersalurnya tamatan untuk bekerja di industry sesuai kompeteni keahliannya.</li>
+            <li>Terbinanya hubungan kerjasama kemitraan saling menguntungkan dengan dunia usaha dunia industri, lembaga diklat serta stake holder yang relevan secara professional.</li>
+            <li>Terbinanya kehidupan yang bersih aqidahnya, benar ibadahnya dan kokoh akhlaknya baik di sekolah maupun di luar sekolah.</li>
+            <li>Terwujudnya kader muda Muhammadiyah yang mempunyai kompetensi dan integritas untuk berperan dalam masyarakat.</li>
+          </ul>
+        </div>
+        </div>
+      </div>
+    </div>
 
   </div>
 </div>
-</div>
-</div>
-</div>
+
 @endsection
